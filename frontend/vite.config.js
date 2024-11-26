f5700e5d29009b9b296538d279421898363ff2d2
Sushma Piska <piskasushma@gmail.com>
2024-11-26 22:57:44 +0530

    vite.config.js updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/FoodDeliveryApp/',
   build: {
     outDir: '../backend/public',
   },
 });
+
+
+
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   build: {
+//     outDir: '../backend/public',
+//   },
+// });
 
 
 
